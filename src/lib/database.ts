faa2d18ef3719ea8833f5c91ba99d7db7c66e301
pranjhi-successive <pranjhi.singh@successive.tech>
2023-12-06 16:02:00 +0530

    A-8

---
+++ src/lib/database.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import countriesData from "./country";
+import { CountryModel } from "../models/Countries";
+// import countriesData from "./country";
+
+class Database {
+  uri: string;
+  constructor() {
+    this.uri = process.env.MONGO_URL ?? "";
+  }
+
+  connect = async (): Promise<void> => {
+    try {
+      await mongoose.connect(this.uri);
+      console.log("connected");
+      await this.seed();
+    } catch (error) {
+      console.log(error);
+      console.error("DATABASE CONNECTION FAILED");
+      process.exit(0);
+    }
+  };
+
+  seed = async (): Promise<void> => {
+    try {
+      await CountryModel.insertMany(countriesData);
+      console.log("seeded succesfully");
+    } catch (error) {
+      console.log("error in seeding", error);
+    }
+  };
+}
+
+export default Database;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import router from "./routes/route";
+import Database from "./lib/database";
 
 interface ServerConfig {
   port: number;
@@ -7,17 +8,20 @@ interface ServerConfig {
   secretKey: string;
   customHeaderName: string;
   customHeaderValue: string;
+  MONGO_URL: string;
 }
-
 class Server {
   private readonly app: express.Application;
   private readonly config: ServerConfig;
 
   constructor(config: ServerConfig) {
+    console.log(config);
     this.config = config;
     this.app = express();
     this.configureMiddleware();
     this.configureRoutes();
+
+    //  await  database.connect();
   }
 
   private configureMiddleware(): any {
@@ -27,6 +31,13 @@ class Server {
   private configureRoutes(): any {
     this.app.use("/", router);
   }
+
+  run = async (): Promise<void> => {
+    const database = new Database();
+    await database.connect();
+    // await database.seed();
+    // console.log(database.seed());
+  };
 
   public listen(): any {
     this.app.listen(this.config.port, () => {
